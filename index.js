b52e6305a4ec360cff43513cf8615e4e89bbd652
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-29 10:06:04 -0700

    processing arguments

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 var args = process.argv.splice(2)
+var resolve = require('cli-path-resolve')
+
+var triggerFiles = []
+
+function commandRun(trigger, args){
+
+}
+
+var commands = {
+	run:commandRun
+}
+
+var found = null
+args.forEach(function(arg){
+	if(found){
+		return
+	}
+	if(commands[arg]){
+		found = arg
+		return
+	}
+	triggerFiles.push(arg)
+})
+
+if(!found){
+	console.error('no command found')
+	process.exit(1)
+}
+
+if(triggerFiles.length<=0){
+	console.error('no trigger files specified')
+	process.exit(1)	
+}
+
+args = args.splice(triggerFiles.length)
 
 console.log('-------------------------------------------');
+console.log('trigger')
+console.dir(triggerFiles)
+console.log('-------------------------------------------');
+console.log('args')
 console.dir(args)
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var tape = require('tape')
 var cp = require('child_process')
+var path = require('path')
 
 function runTrig(t, command, done){
-	command = 'node ' + path.join(__dirname, 'index.js') + ' ' + command
+	command = 'node index.js ' + command
 	cp.exec(command, function(err, stdout, stderr){
 		if(err || stderr){
 			t.fail(err || stderr.toString(), 'run command')
@@ -14,5 +15,13 @@ function runTrig(t, command, done){
 }
 
 tape('get a command from an overriden pipe', function(t){
-	
+	var defaultTriggers = 'test/default.yaml'
+	var overrideTriggers = 'test/overrides.yaml'
+
+	var command = [defaultTriggers, overrideTriggers, 'run', 'info']
+	runTrig(t, command.join(' '), function(err, command){
+		console.log('-------------------------------------------');
+		console.log(command)
+		t.end()
+	})
 })
